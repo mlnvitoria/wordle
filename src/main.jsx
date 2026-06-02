a6f6fca9635b78ec23e74d99785b4d4a3cd113ae
Vitoria Mendes <vitoriamendes95@pm.me>
2026-06-02 16:50:32 -0400

    Turning StrictMode on

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from './App.jsx'
 import Loading from './components/Loading.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <StrictMode>
     <Suspense fallback={<Loading />}>
       <App />
     </Suspense>
-  // </StrictMode>,
+  </StrictMode>,
 )
